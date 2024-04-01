a1db61871181a1e8cf90a86bc7b740d69b83497c
Mahdi Saremi <mahdisarmi00@gmail.com>
2024-04-01 15:23:45 +0430

    0

---
+++ src/Providers/LaplusServiceProvider.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Rapid\Laplus\Providers;
+
+use Illuminate\Support\ServiceProvider;
+
+class LaplusServiceProvider extends ServiceProvider
+{
+
+    public function register()
+    {
+        
+    }
+
+}
