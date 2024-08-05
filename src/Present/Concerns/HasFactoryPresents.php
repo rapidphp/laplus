01acbe85efdf0a7bec54db721c6bfad2452c6f32
Mahdi Saremi <mahdisarmi00@gmail.com>
2024-08-05 17:42:24 +0330

    added factory helpers

---
+++ src/Present/Concerns/HasFactoryPresents.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace Rapid\Laplus\Present\Concerns;
+
+use Illuminate\Support\Facades\Storage;
+
+trait HasFactoryPresents
+{
+
+    public function fileCopy(string $attribute, string $fromFullPath, ?string $to = null)
+    {
+        if (is_null($to))
+        {
+            $extension = @pathinfo($fromFullPath, PATHINFO_EXTENSION);
+            $to = md5(rand(0, PHP_INT_MAX) . '-' . time()) . ($extension ? '.' . $extension : '');
+        }
+
+        $toDisk = $this->modelName()::attr($attribute, 'disk');
+        $toFullPath = Storage::disk($toDisk)->path($to);
+
+        copy($fromFullPath, $toFullPath);
+
+        return $to;
+    }
+
+    public function withFileCopy(string $attribute, string $fromFullPath, ?string $to = null)
+    {
+        return [$attribute => $this->fileCopy($attribute, $fromFullPath, $to)];
+    }
+
+    public function fileMove(string $attribute, string $fromFullPath, ?string $to = null)
+    {
+        if (is_null($to))
+        {
+            $extension = @pathinfo($fromFullPath, PATHINFO_EXTENSION);
+            $to = md5(rand(0, PHP_INT_MAX) . '-' . time()) . ($extension ? '.' . $extension : '');
+        }
+
+        $toDisk = $this->modelName()::attr($attribute, 'disk');
+        $toFullPath = Storage::disk($toDisk)->path($to);
+
+        rename($fromFullPath, $toFullPath);
+
+        return $to;
+    }
+
+    public function withFileMove(string $attribute, string $fromFullPath, ?string $to = null)
+    {
+        return [$attribute => $this->fileMove($attribute, $fromFullPath, $to)];
+    }
+
+}
