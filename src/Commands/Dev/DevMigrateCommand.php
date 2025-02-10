09716fa9ba80139219e8578ee6cf992e18b05332
Mahdi Saremi <mahdisarmi00@gmail.com>
2025-02-10 21:36:16 +0330

    ability to generate guide into dev migrate command

--- src/Commands/Dev/DevMigrateCommand.php
+++ src/Commands/Dev/DevMigrateCommand.php
@@ -8,11 +8,15 @@ use Illuminate\Support\Facades\Artisan;
 class DevMigrateCommand extends Command
 {
 
-    protected $signature = 'dev:migrate {--fresh : Fresh the previous migrations}';
+    protected $signature = 'dev:migrate {--fresh : Fresh the previous migrations} {--guide : Generate the guides}';
     protected $description = 'Generate and run migrations in dev mode';
 
     public function handle()
     {
+        if ($this->option('guide')) {
+            Artisan::call('dev:guide');
+        }
+
         Artisan::call('dev:migration', ['--fresh' => $this->option('fresh')], $this->output);
         Artisan::call('migrate' . ($this->option('fresh') ? ':fresh' : ''), [], $this->output);
     }
