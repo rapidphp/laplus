a6c6ef8fbebf726b03b6658cd508fd6a8568eb21
Mahdi Saremi <mahdisarmi00@gmail.com>
2025-03-28 21:54:48 +0330

    added docblock for register package resource method

--- src/Laplus.php
+++ src/Laplus.php
@@ -3,7 +3,9 @@
 namespace Rapid\Laplus;
 
 use Illuminate\Support\Facades\Facade;
+use Rapid\Laplus\Resources\PackageResource;
 use Rapid\Laplus\Resources\Resource;
+use Rapid\Laplus\Resources\SharedPackageResource;
 
 /**
  * @method static void addResource(string $name, Resource $resource) Add a resource path
@@ -11,6 +13,7 @@ use Rapid\Laplus\Resources\Resource;
  * @method static void loadConfig(array $config) Merge configuration resources
  * @method static Resource[] getResources() Get all resources
  * @method static Resource|null getResource(string $name) Get a resource by name
+ * @method static void registerPackageResource(SharedPackageResource|PackageResource $resource) Register a package resource
  */
 class Laplus extends Facade
 {
